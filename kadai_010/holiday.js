b0320c1c13e685479fb6267a8c9423ad215b4bd2
rotto5 <ichigokani2@gmail.com>
2025-07-26 18:57:08 +0900

    Add files via upload

--- kadai_010/holiday.js
+++ kadai_010/holiday.js
@@ -1,22 +1,38 @@
 const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
 
 // for文の場合
-for(let i = 0; i < 2; i++) {
-  for (let i = 0; i <= 15; i++) {
-  console.log(holidays[i]);
-  }
+for (let i = 0; i <= 15; i++) {
+console.log(holidays[i]);
 }
 
 // while文の場合
 let i = 0;
-while(i < 2) {
-  let j = 0;
-  while (j <= 15) {
-    console.log(holidays[j]);
-    j++;
-  }
+while (i <= 15) {
+  console.log(holidays[i]);
   i++;
 }
+
+
+
+// 間違えて2回繰り返したけど、そういう時はネストすれば出力する
+// // for文の場合
+// for(let i = 0; i < 2; i++) {
+//   for (let i = 0; i <= 15; i++) {
+//   console.log(holidays[i]);
+//   }
+// }
+
+// // while文の場合
+// let i = 0;
+// while(i < 2) {
+//   let j = 0;
+//   while (j <= 15) {
+//     console.log(holidays[j]);
+//     j++;
+//   }
+//   i++;
+// }
+
 
 // // holidays[i]の最後の要素は出力しているか確認
 // for (let i = 0; i <= 15; i++) {
